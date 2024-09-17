3720fffebb3fbeac9d2be665a2be1735bd59698b
Brendan Lackey <brendan.lackey@outlook.com>
2024-09-16 19:49:13 -0700

    Update Phrases

--- scripts/civitai_assistant.py
+++ scripts/civitai_assistant.py
@@ -12,23 +12,23 @@ css_path = os.path.join(os.path.dirname(os.path.abspath(__name__)), "styles.css"
 
 
 def on_ui_tabs():
-    with gr.Blocks(analytics_enabled=False, css=css_path) as civitai_helper_view:
-        with gr.Column(variant="compact", elem_id="civitai_helper_column"):
-            with gr.Row(elem_classes="civitai_helper_row"):
+    with gr.Blocks(analytics_enabled=False, css=css_path) as civitai_assistant_view:
+        with gr.Column(variant="compact", elem_id="civitai_assistant_column"):
+            with gr.Row(elem_classes="civitai_assistant_row"):
                 model_checkboxes = gr.CheckboxGroup(
                     [member.value for member in ModelType],
                     label="Models",
                     info="Select models to update info for",
                     elem_id="ch_model_checkbox_group",
                 )
-            with gr.Row(elem_classes="civitai_helper_row"):
+            with gr.Row(elem_classes="civitai_assistant_row"):
                 with gr.Row():
                     metadata_btn = gr.Button("Update Metadata", elem_id="update_tags_button")
                     # check_updates_btn = gr.Button("Check For Updates", elem_id="check_updates_button")
 
                 metadata_btn.click(fn=update_metadata, inputs=model_checkboxes)
 
-    return [(civitai_helper_view, "Civitai Helper", "civitai_helper_tab")]
+    return [(civitai_assistant_view, "Civitai Assistant", "civitai_assistant_tab")]
 
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
