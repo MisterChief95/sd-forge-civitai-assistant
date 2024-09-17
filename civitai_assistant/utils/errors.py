30aa114fa7edc503780497eedcd1548c2faf7838
Brendan Lackey <brendan.lackey@outlook.com>
2024-09-17 15:59:27 -0700

    add docs

--- civitai_assistant/utils/errors.py
+++ civitai_assistant/utils/errors.py
@@ -1,4 +1,13 @@
 def get_exception_msg(e: Exception) -> str:
+    """
+    Retrieves the message from an exception.
+    Args:
+        e (Exception): The exception from which to extract the message.
+    Returns:
+        str: The message of the exception. If the exception has a 'message' attribute, 
+             it returns that. Otherwise, it returns the string representation of the exception.
+    """
+
     if hasattr(e, "message"):
         return e.message
     else:
